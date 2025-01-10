d2a555f47e3df9ef6ed6bb59d6c5482223679ade
PremBhooma <bhoomasagar1213@gmail.com>
2025-01-11 00:09:18 +0530

    `Added login functionality to user controller`

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -65,3 +65,46 @@ exports.create = async (req, res) => {
     });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find existing admin
+    const existingUser = await User.findOne({ email }).lean();
+    if (!existingUser) {
+      return res.status(StatusCodes.FORBIDDEN).json({
+        errorCode: 1,
+        status: false,
+        message: "Email doesn't exist",
+        data: null,
+      });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, existingUser.password);
+    if (!passwordMatch) {
+      return res.status(StatusCodes.FORBIDDEN).json({
+        errorCode: 2,
+        status: false,
+        message: "Incorrect Password",
+        data: null,
+      });
+    }
+
+    const token = jwt.sign({ userId: existingUser._id }, process.env.JWT_SECRET, { expiresIn: "300d" });
+
+    return res.status(StatusCodes.OK).json({
+      errorCode: 0,
+      status: true,
+      message: "User Login Successfully",
+      data: { ...existingUser, password: undefined, token },
+    });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errorCode: 5,
+      status: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
